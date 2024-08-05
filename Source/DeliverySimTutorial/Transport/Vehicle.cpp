fd60b0cb9573002f5224eff4395e36392b8e8dc0
Misha <mishagalko1@gmail.com>
2024-08-05 14:57:23 +0200

    add pawn and camera movement to vehicle class

--- Source/DeliverySimTutorial/Transport/Vehicle.cpp
+++ Source/DeliverySimTutorial/Transport/Vehicle.cpp
@@ -1,12 +1,65 @@
 #include "Vehicle.h"
-
+#include "ChaosVehicleMovementComponent.h"
 
 AVehicle::AVehicle() {
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraBoom->SetupAttachment(RootComponent);
 
+    Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+    Camera->SetupAttachment(CameraBoom);
 }
 
 void AVehicle::BeginPlay() {
     Super::BeginPlay();
 
     
+}
+
+void AVehicle::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) {
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    PlayerInputComponent->BindAxis("LookRight", this, &AVehicle::LookRight);
+    PlayerInputComponent->BindAxis("LookUp", this, &AVehicle::LookUp);
+
+    PlayerInputComponent->BindAxis("MoveForward", this, &AVehicle::MoveForward);
+    PlayerInputComponent->BindAxis("LurnLeft", this, &AVehicle::LurnLeft);
+    PlayerInputComponent->BindAxis("MoveBack", this, &AVehicle::MoveBack);
+
+    PlayerInputComponent->BindAction("HandBrake", EInputEvent::IE_Pressed, this, &AVehicle::StartHandBrake);
+    PlayerInputComponent->BindAction("HandBrake", EInputEvent::IE_Released, this, &AVehicle::StopHandBrake);
+}
+
+void AVehicle::LookRight(const float Scale)
+{
+    AddControllerYawInput(Scale);
+}
+
+void AVehicle::LookUp(const float Scale)
+{
+    AddControllerPitchInput(Scale);
+}
+
+void AVehicle::MoveForward(const float Scale)
+{
+    GetVehicleMovementComponent()->SetThrottleInput(Scale);
+}
+
+void AVehicle::LurnLeft(const float Scale)
+{
+    GetVehicleMovementComponent()->SetSteeringInput(Scale);
+}
+
+void AVehicle::MoveBack(const float Scale)
+{
+    GetVehicleMovementComponent()->SetBrakeInput(-Scale);
+}
+
+void AVehicle::StartHandBrake()
+{
+    GetVehicleMovementComponent()->SetHandbrakeInput(true);
+}
+
+void AVehicle::StopHandBrake()
+{
+    GetVehicleMovementComponent()->SetHandbrakeInput(false);
 }
--- Source/DeliverySimTutorial/Transport/Vehicle.h
+++ Source/DeliverySimTutorial/Transport/Vehicle.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "WheeledVehiclePawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "Vehicle.generated.h"
 
 /**
@@ -16,6 +18,37 @@ class DELIVERYSIMTUTORIAL_API AVehicle : public AWheeledVehiclePawn
 	
 public:
 	AVehicle();
-
 	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	USpringArmComponent *CameraBoom;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	UCameraComponent *Camera;
+
+public:
+	UFUNCTION()
+	virtual void SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) override;
+
+	UFUNCTION()
+	void LookRight(const float Scale);
+
+	UFUNCTION()
+	void LookUp(const float Scale);
+
+	UFUNCTION()
+	void MoveForward(const float Scale);
+
+	UFUNCTION()
+	void LurnLeft(const float Scale);
+
+	UFUNCTION()
+	void MoveBack(const float Scale);
+
+	UFUNCTION()
+	void StartHandBrake();
+
+	UFUNCTION()
+	void StopHandBrake();
 };
